2ab18db05e3324b406f2fa574b5cdddee48f77ef
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-13 17:09:22 +0900

    元の画像にテキストを合成するメソッド，元の画像にイラストを合成するメソッドを書いた

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -43,7 +43,53 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         photoImageView.image = info[.originalImage] as? UIImage //画像を出力
     }
     
-    
+    //元の画像にテキストを合成するメソッド
+    func drawText(image: UIImage) -> UIImage {
+        
+        let text = "LifeisTech!"
+        
+        //文字の特性　せってい
+        let textFontAttributes = [NSAttributedString.Key.font: UIFont(name: "Arial", size: 120)!, NSAttributedString.Key.foregroundColor: UIColor.red]
+        
+        //Graphicコンテキスト編集開始
+        UIGraphicsBeginImageContext(image.size)
+        
+        let margin: CGFloat = 5.0
+        let textRect = CGRect(x: margin, y: margin, width: image.size.width - margin, height: image.size.height - margin)
+        
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        text.draw(in: textRect, withAttributes: textFontAttributes)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    //元の画像にイラストを合成するメソッド
+    func drawMaskImage(image: UIImage) -> UIImage {
+        
+        let maskImage = UIImage(named: "sakura")
+        
+        UIGraphicsBeginImageContext(image.size)
+        
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        let margin: CGFloat = 50.0
+        
+        let maskRect = CGRect(x: image.size.width - (maskImage?.size.width)! - margin,
+                              y: image.size.height - (maskImage?.size.height)! - margin,
+                              width: (maskImage?.size.width)!,
+                              height: (maskImage?.size.height)!)
+        
+        maskImage?.draw(in: maskRect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
 
 
 }
